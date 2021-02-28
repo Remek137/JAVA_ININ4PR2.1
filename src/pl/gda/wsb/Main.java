ee50c10e92b26c4a37593c868e99335b21461e01
Remek137 <Remeq137@gmail.com>
2021-02-28 09:40:34 +0100

    Hello world

---
+++ src/pl/gda/wsb/Main.java
@@ -0,0 +1,12 @@
+package pl.gda.wsb;
+
+import java.util.Date;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+        Date data = new Date();
+        System.out.println(data);
+    }
+}
