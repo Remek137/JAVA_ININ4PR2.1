b3c954781d14d1c444bda19db19fa40788f7232c
Remek137 <Remeq137@gmail.com>
2021-03-28 17:08:19 +0200

    cars

--- src/pl/gda/wsb/Human.java
+++ src/pl/gda/wsb/Human.java
@@ -1,5 +1,7 @@
 package pl.gda.wsb;
 
+import java.util.Objects;
+
 public class Human {
 
     String firstName;
@@ -18,5 +20,18 @@ public class Human {
         this.isWorking = isWorking;
         this.pet = pet;
         this.car = car;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Human)) return false;
+        Human human = (Human) o;
+        return age == human.age && isWorking == human.isWorking && Objects.equals(firstName, human.firstName) && Objects.equals(lastName, human.lastName) && Objects.equals(position, human.position) && Objects.equals(pet, human.pet) && Objects.equals(car, human.car);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, position, age, isWorking, pet, car);
     }
 }
